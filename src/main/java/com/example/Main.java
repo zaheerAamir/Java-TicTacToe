33c042cf6ed93354e631310c2df079950fb0a20a
zaheerAamir <aamirzaheer95@gmail.com>
2024-06-23 14:50:30 +0530

    Implemented Pattern 🎨

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,5 +6,26 @@ package com.example;
  */
 public class Main {
 
+  public static void main(String[] args) {
+
+    pattern(3, 4);
+
+  }
+
+  static void pattern(int r, int c) {
+
+    for (int row = 1; row <= r; row++) {
+
+      System.out.println("-----------------");
+      // for every row, run the col:
+      for (int col = 1; col <= c; col++) {
+
+        System.out.print("|    ");
+      }
+      System.out.println();
+
+    }
+    System.out.println("-----------------");
+  }
 
 }
