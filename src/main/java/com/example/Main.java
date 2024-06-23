85ceff84dabcef731369b19c0712ed02f0028861
zaheerAamir <aamirzaheer95@gmail.com>
2024-06-23 22:41:34 +0530

    Implemented 2D array to store user input X/O init

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,8 @@
 // Tic Tac Toe Terminal Game:
 package com.example;
 
+import java.util.Scanner;
+
 /**
  * Main
  */
@@ -8,24 +10,56 @@ public class Main {
 
   public static void main(String[] args) {
 
-    pattern(3, 4);
+    if (true) {
+      Scanner reader = new Scanner(System.in);
+      System.out.println("Press 1 to start the game and 0 to exit!: ");
+      int input = reader.nextInt();
+
+      if (input == 0) {
+        reader.close();
+        return;
+      }
+      logic();
+
+      reader.close();
+
+    }
 
   }
 
-  static void pattern(int r, int c) {
+  static void pattern() {
 
-    for (int row = 1; row <= r; row++) {
+    char[][] matrix = new char[3][3];
 
-      System.out.println("-----------------");
+    for (int row = 0; row < matrix.length; row++) {
+
+      System.out.println("-------------");
       // for every row, run the col:
-      for (int col = 1; col <= c; col++) {
+      for (int col = 0; col < matrix[row].length; col++) {
 
-        System.out.print("|    ");
+        System.out.print("|" + row + "," + col);
       }
-      System.out.println();
+      System.out.println("|");
 
     }
-    System.out.println("-----------------");
+
+    System.out.println("-------------");
+  }
+
+  static void logic() {
+    Scanner reader = new Scanner(System.in);
+
+    System.out.println("Logic Initiated!");
+    System.out.println("X will get the first chance to play the game!");
+    pattern();
+
+    System.out.println("Enter the coordinated where you want to put X in the format (row col): ");
+    int r = reader.nextInt();
+    int c = reader.nextInt();
+    System.out.println("You chose: " + r + ", " + c);
+    pattern();
+
+    reader.close();
   }
 
 }
