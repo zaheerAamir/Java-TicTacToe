9bdd98b2c56fa8e1c50373be50b8b27ae0d4b03a
zaheerAamir <aamirzaheer95@gmail.com>
2024-06-30 13:47:29 +0530

    descp: Optimized Game Logic for all edge cases

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,15 +16,17 @@ public class Main {
     System.out.println("Press 1 to start the game and 0 to exit!: ");
     int input = reader.nextInt();
     System.out.println();
-    System.out.println("Game Started!🎮");
-    logic();
 
     while (!exitLoop) {
       if (input == 0) {
         reader.close();
+        System.out.println("Game Ended!");
+        exitLoop = true;
         return;
       }
-      System.out.println();
+
+      System.out.println("Game Started!🎮");
+      logic();
       pattern();
     }
 
@@ -53,10 +55,55 @@ public class Main {
     System.out.println("-------------");
   }
 
+  static boolean checkWonOrDraw(String choise) {
+    // Horizontal:
+    if (matrix[0][0].equals(choise) && matrix[0][1].equals(choise) && matrix[0][2].equals(choise)) {
+      System.out.println("Game Ended " + choise + " Won!");
+
+      return true;
+    }
+    if (matrix[1][0].equals(choise) && matrix[1][1].equals(choise) && matrix[1][2].equals(choise)) {
+      System.out.println("Game Ended " + choise + " Won!");
+
+      return true;
+    }
+    if (matrix[2][0].equals(choise) && matrix[2][1].equals(choise) && matrix[2][2].equals(choise)) {
+      System.out.println("Game Ended " + choise + " Won!");
+
+      return true;
+    }
+    // Vertical:
+    if (matrix[0][0].equals(choise) && matrix[1][0].equals(choise) && matrix[2][0].equals(choise)) {
+      System.out.println("Game Ended " + choise + " Won!");
+
+      return true;
+    }
+    if (matrix[0][1].equals(choise) && matrix[1][1].equals(choise) && matrix[2][1].equals(choise)) {
+      System.out.println("Game Ended " + choise + " Won!");
+
+      return true;
+    }
+    if (matrix[0][2].equals(choise) && matrix[1][2].equals(choise) && matrix[2][2].equals(choise)) {
+      System.out.println("Game Ended " + choise + " Won!");
+
+      return true;
+    }
+    // Diagonal:
+    if (matrix[0][0].equals(choise) && matrix[1][1].equals(choise) && matrix[2][2].equals(choise)) {
+      System.out.println("Game Ended " + choise + " Won!");
+
+      return true;
+    }
+    if (matrix[0][2].equals(choise) && matrix[1][1].equals(choise) && matrix[2][0].equals(choise)) {
+      System.out.println("Game Ended " + choise + " Won!");
+
+      return true;
+    }
+    return false;
+  }
+
   static void pattern() {
     Scanner reader = new Scanner(System.in);
-
-
 
     System.out.println("Enter the coordinates where you want to put X in the format (row col): ");
     int r = reader.nextInt();
@@ -64,53 +111,55 @@ public class Main {
 
     if (r > 2 || c > 2) {
       System.out.println("row or col no. not found!");
+      System.out.println();
+
+      System.out.println("Enter the coordinates where you want to put X in the format (row col): ");
+      r = reader.nextInt();
+      c = reader.nextInt();
+    }
+
+    // Checking if X or O exist at r,c or not. if not then add O.
+    if (!matrix[r][c].equals("X") && !matrix[r][c].equals("O")) {
+      matrix[r][c] = "X";
+    } else {
+      System.out.println(matrix[r][c] + " already exists at " + r + "," + c);
+      System.out.println("Please choose a different position!");
+      System.out.println();
+
+      System.out.println("Enter the coordinates where you want to put X in the format (row col): ");
+      r = reader.nextInt();
+      c = reader.nextInt();
+
+      matrix[r][c] = "X";
+
+    }
+    logic();
+
+    // check won or draw
+    boolean resX = checkWonOrDraw("X");
+    if (resX) {
+      exitLoop = true;
       return;
     }
 
-    matrix[r][c] = "X";
-    logic();
-    // Horizontal:
-    if (matrix[0][0].equals("X") && matrix[0][1].equals("X") && matrix[0][2].equals("X")) {
-      System.out.println("Game Ended X Won!");
-      exitLoop = true;
-      return;
+    int countX = 0;
+    // Check for Draw:
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[row].length; col++) {
+
+        if (matrix[row][col] == "X" || matrix[row][col] == "O" && countX != 9) {
+          countX = countX + 1;
+
+        }
+
+      }
+
     }
-    if (matrix[1][0].equals("X") && matrix[1][1].equals("X") && matrix[1][2].equals("X")) {
-      System.out.println("Game Ended X Won!");
+    if (countX == 9) {
+      System.out.println("Match is Draw!");
       exitLoop = true;
       return;
-    }
-    if (matrix[2][0].equals("X") && matrix[2][1].equals("X") && matrix[2][2].equals("X")) {
-      System.out.println("Game Ended X Won!");
-      exitLoop = true;
-      return;
-    }
-    // Vertical:
-    if (matrix[0][0].equals("X") && matrix[1][0].equals("X") && matrix[2][0].equals("X")) {
-      System.out.println("Game Ended X Won!");
-      exitLoop = true;
-      return;
-    }
-    if (matrix[0][1].equals("X") && matrix[1][1].equals("X") && matrix[2][1].equals("X")) {
-      System.out.println("Game Ended X Won!");
-      exitLoop = true;
-      return;
-    }
-    if (matrix[0][2].equals("X") && matrix[1][2].equals("X") && matrix[2][2].equals("X")) {
-      System.out.println("Game Ended X Won!");
-      exitLoop = true;
-      return;
-    }
-    // Diagonal:
-    if (matrix[0][0].equals("X") && matrix[1][1].equals("X") && matrix[2][2].equals("X")) {
-      System.out.println("Game Ended X Won!");
-      exitLoop = true;
-      return;
-    }
-    if (matrix[0][2].equals("X") && matrix[1][1].equals("X") && matrix[2][0].equals("X")) {
-      System.out.println("Game Ended X Won!");
-      exitLoop = true;
-      return;
+
     }
 
 
@@ -120,53 +169,56 @@ public class Main {
 
     if (r > 2 || c > 2) {
       System.out.println("row or col no. not found!");
+      System.out.println();
+
+      System.out.println("Enter the coordinates where you want to put O in the format (row col): ");
+      r = reader.nextInt();
+      c = reader.nextInt();
+    }
+
+    // Checking if X or O exist at r,c or not. if not then add O.
+    if (!matrix[r][c].equals("X") && !matrix[r][c].equals("O")) {
+      matrix[r][c] = "O";
+    } else {
+      System.out.println(matrix[r][c] + " already exists at " + r + "," + c);
+      System.out.println("Please choose a different position!");
+      System.out.println();
+
+      System.out.println("Enter the coordinates where you want to put O in the format (row col): ");
+      r = reader.nextInt();
+      c = reader.nextInt();
+
+      matrix[r][c] = "O";
+
+    }
+    logic();
+
+    // Check won or draw
+    boolean resO = checkWonOrDraw("O");
+    if (resO) {
+      exitLoop = true;
       return;
     }
 
-    matrix[r][c] = "O";
-    logic();
-    // Horizontal:
-    if (matrix[0][0].equals("O") && matrix[0][1].equals("O") && matrix[0][2].equals("O")) {
-      System.out.println("Game Ended O Won!");
-      exitLoop = true;
-      return;
+    int count = 0;
+    // Check for Draw:
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[row].length; col++) {
+
+        if (matrix[row][col] == "X" || matrix[row][col] == "O" && count != 9) {
+          count = count + 1;
+
+        }
+
+      }
+
     }
-    if (matrix[1][0].equals("O") && matrix[1][1].equals("O") && matrix[1][2].equals("O")) {
-      System.out.println("Game Ended O Won!");
+
+    if (count == 9) {
+      System.out.println("Game Ended Match Draw!");
       exitLoop = true;
       return;
-    }
-    if (matrix[2][0].equals("O") && matrix[2][1].equals("O") && matrix[2][2].equals("O")) {
-      System.out.println("Game Ended O Won!");
-      exitLoop = true;
-      return;
-    }
-    // Vertical:
-    if (matrix[0][0].equals("O") && matrix[1][0].equals("O") && matrix[2][0].equals("O")) {
-      System.out.println("Game Ended O Won!");
-      exitLoop = true;
-      return;
-    }
-    if (matrix[0][1].equals("O") && matrix[1][1].equals("O") && matrix[2][1].equals("O")) {
-      System.out.println("Game Ended O Won!");
-      exitLoop = true;
-      return;
-    }
-    if (matrix[0][2].equals("O") && matrix[1][2].equals("O") && matrix[2][2].equals("O")) {
-      System.out.println("Game Ended O Won!");
-      exitLoop = true;
-      return;
-    }
-    // Diagonal:
-    if (matrix[0][0].equals("O") && matrix[1][1].equals("O") && matrix[2][2].equals("O")) {
-      System.out.println("Game Ended O Won!");
-      exitLoop = true;
-      return;
-    }
-    if (matrix[0][2].equals("O") && matrix[1][1].equals("O") && matrix[2][0].equals("O")) {
-      System.out.println("Game Ended O Won!");
-      exitLoop = true;
-      return;
+
     }
 
   }
